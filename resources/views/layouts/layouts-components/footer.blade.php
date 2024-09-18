132bb07eafe6e1eec41429590e0bdc1aabd12cfd
KacperCLK <kacper.celak@gmail.com>
2024-09-18 20:02:58 +0200

    modified:   resources/views/layouts/layouts-components/footer.blade.php

--- resources/views/layouts/layouts-components/footer.blade.php
+++ resources/views/layouts/layouts-components/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer">
-    &copy; {{ date('Y') }} Made by: <a href="https://portfolio.kacpercelak.pl/home" class="link link--underline"><p>Kacper Celak.</p></a>All rights reserved.
+    &copy; {{ date('Y') }} Made by: <a href="https://kacpercelak.pl" class="link link--underline"><p>Kacper Celak.</p></a>All rights reserved.
 </footer>
